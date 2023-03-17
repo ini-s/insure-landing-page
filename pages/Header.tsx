6fb89f65556d691769ac5428360c1b1a5bf4c7b0
IMS <inisoetan2014@gmail.com>
2023-03-17 09:19:57 +0000

    feat: add side nav bar

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -1,7 +1,12 @@
 import styles from "../styles/Header.module.css"
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Header() {
+    const [active, setIsActive] = useState<boolean>(false)
+    function handleClick(e: React.MouseEvent<HTMLElement>) {
+        setIsActive(prevValue => !prevValue)
+    }
     return (
         <header className={styles.header}>
             <Image
@@ -13,14 +18,51 @@ export default function Header() {
             />
             {/* <nav className={styles.mainNav}>
             </nav> */}
-            <Image
-                src="/images/icon-hamburger.png"
-                className={styles.hamburger}
-                alt="hamburger-menu"
-                width={32}
-                height={32}
-            />
-            {/* <nav className={styles.sideNav}></nav> */}
+            {
+                !active ?
+                    <Image
+                        src="/images/icon-hamburger.png"
+                        className={styles.hamburger}
+                        alt="hamburger-menu"
+                        width={32}
+                        height={32}
+                        onClick={handleClick}
+                    />
+                    :
+                    <>
+                        <Image
+                            src="/images/icon-close.png"
+                            className={styles.close}
+                            alt="hamburger-menu"
+                            width={32}
+                            height={32}
+                            onClick={handleClick}
+                        />
+                        <nav className={styles.sideNav}>
+                            <ul>
+                                <li className={styles.navList}>
+                                    <a>How we work</a>
+                                </li>
+                                <li className={styles.navList}>
+                                    <a> Blog</a>
+                                </li>
+                                <li className={styles.navList}>
+                                    <a>Account</a>
+                                </li>
+                            </ul>
+                            <button className={styles.plansBtn}>
+                                View plans
+                            </button>
+                            <Image
+                                src="/images/bg-pattern-mobile-nav.png"
+                                className={styles.bgMobileNav}
+                                alt="bg-pattern-mobile-nav"
+                                width={375}
+                                height={218}
+                            />
+                        </nav>
+                    </>
+            }
         </header>
     )
 }
